da1fe5e30ce1dc2ce967f2b10bbe6a92c2d6fd51
tin0312 <105316547+tin0312@users.noreply.github.com>
2025-09-30 13:16:42 -0400

    Fixed cart layout

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 
 export default function CartPage() {
   const { cart, addToCart, removeFromCart, clearCart } = useCart();
-  
+
   //get total price
   let totalPrice = cart.reduce(
     (sum, drink) => sum + drink.price * drink.numberOfOrders,
@@ -35,31 +35,44 @@ export default function CartPage() {
   }
 
   return (
-    <div className="p-6">
+    <div className="p-29 flex flex-col items-center h-screen justify-center">
       <h1 className="text-2xl font-bold">Your Cart</h1>
       {cart.map((drink) => (
-        <div key={drink.id} className="flex items-center gap-4 my-2">
-            <Image
-                src={drink.image}
-                alt={drink.name}
-                width={300}
-                height={300}
-                className="rounded-xl shadow-lg"
-              />
+        <div
+          key={drink.id}
+          className="flex items-center justify-center gap-4 my-2"
+        >
+          <Image
+            src={drink.image}
+            alt={drink.name}
+            width={50}
+            height={50}
+            className="rounded-xl shadow-lg"
+          />
           <p>{drink.name}</p>
-          <p>(${drink.price} × {drink.numberOfOrders})</p>
+          <p>
+            (${drink.price} × {drink.numberOfOrders})
+          </p>
           <p>({drink.numberOfOrders})</p>
           <button onClick={() => addToCart(drink)}>+</button>
           <button onClick={() => removeFromCart(drink.id)}>-</button>
         </div>
       ))}
       <p>Total: {totalPrice}</p>
-      <button onClick={clearCart} className="mt-4 bg-red-500 text-white px-4 py-2 rounded">
-        Clear Cart
-      </button>
-      <button onClick={handleOrder} className="mt-4 bg-green-500 text-white px-4 py-2 rounded">
-        Order
-      </button>
+      <div className="flex gap-1.5">
+        <button
+          onClick={clearCart}
+          className="mt-4 bg-red-500 text-white px-4 py-2 rounded"
+        >
+          Clear Cart
+        </button>
+        <button
+          onClick={handleOrder}
+          className="mt-4 bg-green-500 text-white px-4 py-2 rounded"
+        >
+          Order
+        </button>
+      </div>
     </div>
   );
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Footer() {
     return (
-        <footer className="w-full border-t bg-white text-center p-4 text-sm text-gray-500">
+        <footer className="absolute bottom-0 w-full border-t bg-white text-center p-4 text-sm text-gray-500">
         &copy; {new Date().getFullYear()} The 26 apt Cafe. All rights reserved.
         </footer>
     );
